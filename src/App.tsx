7d4e35f706b70d73f9ca2d8efdbd07cede1a9274
killer-cf <costa.kilder@live.com>
2023-02-23 11:08:26 -0300

    feat: global style and default theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
-import './App.css'
+import { ThemeProvider } from 'styled-components'
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
 
 function App() {
-  return <div>Hello world</div>
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <h1>Hello world</h1>
+
+      <GlobalStyle />
+    </ThemeProvider>
+  )
 }
 
 export default App
